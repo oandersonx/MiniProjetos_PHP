55df16f83acae55dd5851a4eb631b00995487a06
Anderson <anderson.jribeiro1@gmail.com>
2022-01-06 03:13:32 -0300

    ajustando SESSION

--- Sessoes/home.php
+++ Sessoes/home.php
@@ -2,13 +2,13 @@
 
     session_start();
 
-    $nome = filter_input(INPUT_POST, "nome", FILTER_SANITIZE_SPECIAL_CHARS); // Evita códigos no input 
-    $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
-    $idade = filter_input(INPUT_POST, "idade", FILTER_VALIDATE_INT); 
+    $_SESSION['nome'] = filter_input(INPUT_POST, "nome", FILTER_SANITIZE_SPECIAL_CHARS); // Evita códigos no input 
+    $_SESSION['email'] = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
+    $_SESSION['idade'] = filter_input(INPUT_POST, "idade", FILTER_VALIDATE_INT); 
 
-    if ($nome && $email && $idade){
-        echo 'Olá, '."<b>".$nome."</b>".', seu email é '."<b>".$email."</b>".' e sua idade é '."<b>".$idade."<b>";
-        echo "<br/><i>"."Estes dados vieram através de sessões."."</i>";
+    if ($_SESSION['nome'] && $_SESSION['email'] && $_SESSION['idade']){
+        echo 'Olá, '."<b>".$_SESSION['nome']."</b>".', seu email é '."<b>".$_SESSION['email']."</b>".' e sua idade é '."<b>".$_SESSION['idade']."<b>";
+        echo "<br/><i>"."Estes dados foram armazenados em uma sessão e exibidos aqui."."</i>";
     }else{
        $_SESSION['mensagem_erro'] = 'Digite os dados corretamente!';
        header("Location: sessoes.php");
